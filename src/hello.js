49ae8750f6fe27c32a5e6b4cab09c5ef0b28722d
juliswer <julianswer36@gmail.com>
2022-04-17 18:36:22 -0300

    updateTask function

--- src/hello.js
+++ src/hello.js
@@ -1,11 +1,14 @@
 "use strict";
 
 module.exports.hello = async (event) => {
+
+  const message = 'This is a app made by Julian Swerdlin using AWS Lambda and AWS API Gateway (dynamodb, serverless and other tools) code: https://github.com/juliswer/aws-lambda-dynamodb-crud-node';
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "hello world",
+        message,
         input: event,
       },
       null,
